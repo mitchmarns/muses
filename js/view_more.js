6f4155662dac8a00cfc50ccc0af7865bc0e6d93c
mitchmarns <sakurapizzaa@gmail.com>
2025-01-04 12:58:06 -0800

    Update view_more.js

--- js/view_more.js
+++ js/view_more.js
@@ -14,4 +14,30 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById(targetTab).classList.add('active');
         });
     });
+
+// Popup Modal functionality
+    const images = document.querySelectorAll('#instagram-gallery img');
+    const modal = document.getElementById('image-modal');
+    const modalImg = document.getElementById('modal-image');
+    const captionText = document.getElementById('caption');
+    const closeBtn = document.querySelector('.close');
+
+    images.forEach(image => {
+        image.addEventListener('click', () => {
+            modal.style.display = 'block';
+            modalImg.src = image.src;
+            captionText.textContent = image.alt;
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    // Close modal when clicking outside the image
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
 });
